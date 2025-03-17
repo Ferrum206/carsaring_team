c7c592c74e5ba4b771b4a8610836ccdcc8ff5a36
Nurlan <nurikkg060@gmail.com>
2025-03-17 09:58:57 +0600

    подключил шрифт

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import localFont from 'next/font/local';
+
+const stolzlFont = localFont({
+    src: [
+        {
+            path: './fonts/Stolzl-Light.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: './fonts/Stolzl-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: './fonts/Stolzl-Medium.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: './fonts/Stolzl-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        }
+    ]
+})
+
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html>
+        <body className={stolzlFont.className}>
+            {children}
+        </body>
+    </html>
+  )
+}
