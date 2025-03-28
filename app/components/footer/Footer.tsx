b91d53552822a89a2ef820b7a77c5468927ee2a2
tempo <temirlanbelekov91@gmail.com>
2025-03-28 23:33:58 +0600

    footer end (finally)

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -51,76 +51,154 @@ export default function Footer() {
           </SwiperSlide>
         </Swiper>
       </div>
-      <div className={styles.generalLinks}>
-        <div className={styles.footerLinks}>
-          <p className={styles.forCustomer}>For Customers</p>
-          <Link href="/about" className={styles.links}>
-            About Us
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Conditions
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Testimonials
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Articles
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Contacts
-          </Link>
-        </div>
-        <div className={styles.footerLinks}>
-          <p className={styles.forCustomer}>Car List</p>
-          <Link href="/about" className={styles.links}>
-            SUVs
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Convertibles
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Sports Cars
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Premium
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Coupe
-          </Link>
-        </div>
-        <div className={styles.footerLinks}>
-          <p className={styles.forCustomer}>Service</p>
-          <Link href="/about" className={styles.links}>
-            Car List
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Yacht List
-          </Link>
-          <Link href="/about" className={styles.links}>
-            Chauffeur
-          </Link>
+      <div className={styles.footerInfo}>
+        <div className={styles.generalLinks}>
+          <div className={styles.linksGroup}>
+            <div className={styles.footerLinks}>
+              <p className={styles.forCustomer}>For Customers</p>
+              <Link href="/about" className={styles.links}>
+                About Us
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Conditions
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Testimonials
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Articles
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Contacts
+              </Link>
+            </div>
+            <div className={styles.footerLinks}>
+              <p className={styles.forCustomer}>Car List</p>
+              <Link href="/about" className={styles.links}>
+                SUVs
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Convertibles
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Sports Cars
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Premium
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Coupe
+              </Link>
+            </div>
+            <div className={styles.footerLinks}>
+              <p className={styles.forCustomer}>Service</p>
+              <Link href="/about" className={styles.links}>
+                Car List
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Yacht List
+              </Link>
+              <Link href="/about" className={styles.links}>
+                Chauffeur
+              </Link>
+            </div>
+          </div>
+          <div className={styles.contacts}>
+            <div className={styles.numberTwo}>
+              +971 58 590 7875
+              <div className={styles.social}>
+                <Link href="/telegram">
+                  <Image
+                    src="/Telegram.svg"
+                    width={22}
+                    height={21}
+                    alt="telegram"
+                    className={styles.imageLogoTele}
+                  />
+                </Link>
+                <Link href="/whatsapp">
+                  <Image
+                    src="/WhatsApp.svg"
+                    width={22}
+                    height={21}
+                    alt="whatsapp"
+                    className={styles.imageLogoWhats}
+                  />
+                </Link>
+              </div>
+            </div>
+
+            <button className={styles.btn}>REQUEST A CALL BACK</button>
+            <p className={styles.dubai}>
+              24 4th St - Al Quoz - Al Quoz Industrial Area 3 - Dubai
+            </p>
+            <div className={styles.inputMain}>
+              <input
+                type="email"
+                placeholder="Write your E-mail"
+                className={styles.input}
+              />
+              <button className={styles.button}>SUBMIT</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.contacts}>
-        <div className={styles.numberTwo}>+971 58 590 7875 </div>
-        <Link href="/telegram">
+      <div className={styles.paymentContainer}>
+        <div className={styles.payment}>
+          <Image src="/VISA.svg" width={90} height={27} alt="visa"></Image>
           <Image
-            src="/Telegram.svg"
-            width={22}
-            height={21}
-            alt="telegram"
-            className={styles.imageLogoTele}
-          />
-        </Link>
-        <Link href="/whatsapp">
+            src="/Master-card.svg"
+            width={78}
+            height={43}
+            alt="visa"
+          ></Image>
           <Image
-            src="/WhatsApp.svg"
-            width={22}
-            height={21}
-            alt="whatsapp"
-            className={styles.imageLogoWhats}
-          />
+            src="/American-express.svg"
+            width={99}
+            height={39}
+            alt="visa"
+          ></Image>
+          <Image src="/Union-pay.svg" width={72} height={44} alt="visa"></Image>
+          <Image src="/tether.svg" width={124} height={27} alt="visa"></Image>
+          <Image
+            src="/Google-pay.svg"
+            width={92}
+            height={36}
+            alt="visa"
+          ></Image>
+          <Image src="/apple-pay.svg" width={85} height={41} alt="visa"></Image>
+          <Image src="/giropay.svg" width={84} height={36} alt="visa"></Image>
+          <Image src="/tether.svg" width={124} height={27} alt="visa"></Image>
+          <Image src="/sepa.svg" width={117} height={26} alt="visa"></Image>
+          <Image src="/safetypay.svg" width={93} height={65} alt="visa"></Image>
+        </div>
+      </div>
+      <div className={styles.footerEnd}>
+        <Link href="#" className={styles.privat}>
+          Privacy Policy
         </Link>
+        <p className={styles.rights}>©2023 TRINITY. All rights reserved</p>
+        <div className={styles.socialLinks}>
+          <Image
+            src="/Facebook.svg"
+            width={26}
+            height={26}
+            alt="facebook"
+          ></Image>
+          <Image src="/TikTok.svg" width={26} height={26} alt="Tiktok"></Image>
+          <Image
+            src="/youtube.svg"
+            width={26}
+            height={26}
+            alt="YouTube"
+          ></Image>
+          <Image
+            src="/instagram.svg"
+            width={22}
+            height={22}
+            alt="Instagram"
+          ></Image>
+        </div>
       </div>
     </footer>
   );
