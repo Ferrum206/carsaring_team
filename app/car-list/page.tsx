cd1c32eba3c4c7a7afdf64e85beef2ad775004e9
Alishan <alishanshadmonov@gmail.com>
2025-04-01 10:11:29 +0600

    добавил в,public папку foto.car

--- app/car-list/page.tsx
+++ app/car-list/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function HomePage() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>fsdfdsfsdfs</h1>
+      <h1 className={styles.title}></h1>
       </div>
   )
 }
