0e7147b4e9a5c158249b2b3ee7428bae59b06d16
Alishan <alishanshadmonov@gmail.com>
2025-04-01 09:08:00 +0600

    add page car list

---
+++ app/car-list/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css"; 
+// "../app/styles/page.module.css"
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>fsdfdsfsdfs</h1>
+      </div>
+  )
+}
